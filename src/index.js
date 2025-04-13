d185f192dbb241f4fa68299d0f4b0d468338935a
Sayam Patel <sayamparejiya@gmail.com>
2025-04-13 23:48:50 +0530

    utilities  code discussion

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-// require("dotenv").config({path: './env'});
-import dotenv from "dotenv";
+// configuration of env using require syntax
+// require("dotenv").config({path: './env'}); 
 
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
 
+    // generally before the "listen" listener, we add the "error" listener for error handling
+    app.on("error", (error) => {
+        console.error("Error: ", error);
+        throw error;
+    });
 
-
-
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch(error => {
+    console.error("Mongodb connection failed", error);    
+})
 
 
 
 
 /*
+// this is one type of method to connect to mongodb using mongoose
+
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants";
 import express from "express"
@@ -27,6 +41,7 @@ const app = express();
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
+        // generally before the "listen" listener, we add the "error" listener for error handling
         app.on("error", (error) => {
             console.error("Error: ", error);
             throw error;
